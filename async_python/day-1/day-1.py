839d6b28f67dd95796aad125f8f33b42583dd931
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-12-16 02:30:17 +0500

    day-1

---
+++ async_python/day-1/day-1.py
@@ -0,0 +1,26 @@
+# Асинхронный Python (синхронность vs асинхронность).
+import asyncio
+
+
+# Задача: Эмуляция скачивания файлов
+import time
+
+#sinxronnaya verisya
+# def download_file():
+#     print("Downloading file1...")
+#     time.sleep(3)
+#     print("file1 downloaded!")
+#     print("Downloading file2...")
+#     time.sleep(2)
+#     print("file2 downloaded!")
+#     print("Downloading file3...")
+#     time.sleep(1)
+#     print("file3 downloaded!")
+# completed!
+
+#asinxronnaya verisya
+async def download_file():
+...
+
+
+run = download_file()
