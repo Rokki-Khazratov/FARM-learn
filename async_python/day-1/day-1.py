a33f01b1c512a44e16fa058d8006e9ee1ed7a2d2
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-12-16 02:34:45 +0500

    final

--- async_python/day-1/day-1.py
+++ async_python/day-1/day-1.py
@@ -1,12 +1,9 @@
-# Асинхронный Python (синхронность vs асинхронность).
 import asyncio
-
-
-# Задача: Эмуляция скачивания файлов
 import time
 
-# sinxronnaya verisya
+# Синхронная версия
 def download_file():
+    start_time = time.perf_counter()  # Засекаем время
     print('sync')
     print("Downloading file1...")
     time.sleep(3)
@@ -17,22 +14,25 @@ def download_file():
     print("Downloading file3...")
     time.sleep(1)
     print("file3 downloaded!")
+    end_time = time.perf_counter()  # Засекаем время
+    print(f"Sync download completed in {end_time - start_time:.2f} seconds")
 
-run = download_file()
-
-
-#asinxronnaya verisya
+# Асинхронная версия
 async def async_download_file():
+    start_time = time.perf_counter()  # Засекаем время
     print('Async')
     print("Downloading file1...")
-    await asyncio.sleep(3) 
+    await asyncio.sleep(3)
     print("file1 downloaded!")
     print("Downloading file2...")
-    await asyncio.sleep(2) 
+    await asyncio.sleep(2)
     print("file2 downloaded!")
     print("Downloading file3...")
-    await asyncio.sleep(1) 
+    await asyncio.sleep(1)
     print("file3 downloaded!")
+    end_time = time.perf_counter()  # Засекаем время
+    print(f"Async download completed in {end_time - start_time:.2f} seconds")
 
-
+# Запуск
+download_file()
 asyncio.run(async_download_file())
